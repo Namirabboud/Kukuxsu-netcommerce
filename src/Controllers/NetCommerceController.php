5db01dc2e5dc600861f0fdad07a2e4d22ecff6ad
namir abboud <namir.abboud@tedmob.com>
2021-02-23 17:10:38 +0200

    update

--- src/Controllers/NetCommerceController.php
+++ src/Controllers/NetCommerceController.php
@@ -6,6 +6,7 @@ use Exception;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use App\Http\Traits\NetcommerceTrait;
 
 class NetCommerceController extends Controller
