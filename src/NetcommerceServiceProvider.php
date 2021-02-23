5f26178ac0ba55e2e31f57d2f43bff17e7d8b510
namir abboud <namir.abboud@tedmob.com>
2021-02-23 18:24:54 +0200

    update

--- src/NetcommerceServiceProvider.php
+++ src/NetcommerceServiceProvider.php
@@ -18,7 +18,7 @@ class NetcommerceServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'base');
+        $this->loadViewsFrom(__DIR__.'/Views', 'base');
 
 
         $this->publishes([
