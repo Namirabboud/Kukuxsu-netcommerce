df33314b8ee4c61d9a85ed4eab2f297936d53469
namir abboud <namir.abboud@tedmob.com>
2021-02-23 17:12:33 +0200

    update

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
-Route::get('netcommerce-redirect-payment/{id}', 'NetCommerceController@redirectPayment')->name('netcommerce.payment.redirect');
-Route::post('netcommerce-payment-response', 'NetCommerceController@paymentResponse')->name('netcommerce.payment.response');
+Route::get('netcommerce-redirect-payment/{id}', 'Kukuxsu\Netcommerce\NetCommerceController@redirectPayment')->name('netcommerce.payment.redirect');
+Route::post('netcommerce-payment-response', 'Kukuxsu\Netcommerce\NetCommerceController@paymentResponse')->name('netcommerce.payment.response');
 Route::get('payment-mobile-response', function(){
     echo 'Payment Response';
 })->name('payment-mobile-response');
